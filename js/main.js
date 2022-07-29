15e5268928e24c9863270b29f101c01d84ef60ca
Fatih Sahin <s73121@beuth-hochschule.de>
2022-07-29 10:50:53 +0200

    Setup done

--- js/main.js
+++ js/main.js
@@ -146,3 +146,35 @@ var repeat = function (activeClass) {
   repeater();
 };
 repeat();
+
+// SWIPER
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  fade: "true",
+  grabCursor: "true",
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
